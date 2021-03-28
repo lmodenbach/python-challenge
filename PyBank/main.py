084cfab238716baf5e94af67fcd1ba1fba8724bb
lmodenbach <lmodenbach@gmail.com>
2021-03-28 14:47:45 -0700

    list of objectives added to both scripts
    list of objectives with translations into what the program must do to achieve them

---
+++ PyBank/main.py
@@ -0,0 +1,7 @@
+"""Objectives:
+-total num months: total count rows
+-net total profits/losses: sum column B
+-changes in profits/losses with avg: diff between column B of current row and that of row before with avg diff
+-greatest increase in profits: max of changes in profits/losses
+-greatest decrease in losses: min of changes in profits/losses
+"""
---
+++ PyPoll/main.py
@@ -0,0 +1,8 @@
+"""Objectives:
+-total num votes cast: count of rows
+-list of candidates: list of unique string values in column C
+-total num of votes per candidate: count of instances per each unique value in C
+-percentage of vote won by candidate: (each count of instances over row count)*100
+-winner of election based on pop vote: max of units of a data structure holding count of instances
+    of unique values in C
+ """
