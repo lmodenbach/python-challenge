75457055f611d2eaf539d14587b8acd7971301aa
lmodenbach <lmodenbach@gmail.com>
2021-03-28 15:48:26 -0700

    initial code for both main.py
    added path variables, function to print to terminal, opening of source file and beginning skeleton of for loop to iterate through the file

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,3 +7,29 @@
 -greatest decrease in losses: min of changes in profits/losses
 -output to python-challenge/PyBank/PyBank_results.txt
 """
+
+#create input variable and store path to source csv
+budget_data_csv = os.path.join("..", "Resources", "PyBank", "budget_data.csv")
+
+#create output variable and store path to .txt file
+output_path = os.path.join("PyBank_results.txt")
+
+#define function to output data, takes a list of results with desired data in different indices
+def budget_analysis(budget_data):
+    print(f"Financial Analysis\n")
+    print(f"-------------------------------\n")
+    print(f"Total Months: {budget_data[0]}\n")
+    print(f"Total: ${budget_data[1]}\n")
+    print(f"Average Change: ${budget_data[2]}\n")
+    print(f"Greatest Increase in profits: {budget_data[3]} (${budget_data[4]})\n")
+    print(f"Greatest Decrease in profits: {budget_data[5]} (${budget_data[6]})\n")
+    print(f"-----------------------------------------------------------------------------\n")
+
+#pass in path variable and open up csv file for reading
+with open(budget_dat_csv, 'r') as csvFile:
+
+#specify a reader using commas as delimiter
+    csvReader = csv.reader(csvFile, delimiter=',')
+
+#for loop to iterate through rows in csv file
+    for row in csvReader:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,3 +8,29 @@
     of unique values in C
 -output results to python-challenge/PyPoll/PyPoll_results.txt
  """
+
+#create input variable and store path to source csv
+election_data_csv = os.path.join("..", "Resources", "PyPoll", "election_data.csv")
+
+ #create output variable and store path to .txt file
+output_path = os.path.join("PyPoll_results.txt")
+
+#define function to output data, takes 3 lists of results with desired data in corresponding indices
+def election_analysis(candidate_data, percent_data, totals_data):
+    print(f"Election Results\n")
+    print(f"-------------------------------\n")
+    print(f"Total Votes: {election_data[0]}\n")
+    print(f"{candidate_data[1]}: {percent_data[1]} ({totals_data[3]}\n")
+    print(f"{candidate_data[2]}: {percent_data[2]} ({totals_data[6]}\n")
+    print(f"{candidate_data[3]}: {percent_data[3]} ({totals_data[9]}\n")
+    print(f"-----------------------------------------------------------------------------\n")
+
+
+#pass in path variable and open up csv file for reading
+with open(election_data_csv, 'r') as csvFile:
+
+#specify a reader using commas as delimiter
+    csvReader = csv.reader(csvFile, delimiter=',')
+
+#for loop to iterate through rows in csv file
+    for row in csvReader:
