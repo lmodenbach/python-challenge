506eb304c4d20d44a331a749a398f50cd63cb79b
lmodenbach <lmodenbach@gmail.com>
2021-03-28 14:58:01 -0700

    update the objectives with input/output specs

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,9 @@
 """Objectives:
+-read in from python-challenge/Resources/PyBank/budget_data.csv
 -total num months: total count rows
 -net total profits/losses: sum column B
 -changes in profits/losses with avg: diff between column B of current row and that of row before with avg diff
 -greatest increase in profits: max of changes in profits/losses
 -greatest decrease in losses: min of changes in profits/losses
+-output to python-challenge/PyBank/PyBank_results.txt
 """
