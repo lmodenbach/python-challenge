84c0698e6d2485d9358494668b2774bfd062e79e
lmodenbach <lmodenbach@gmail.com>
2021-04-02 12:40:10 -0700

    finished precoding PyBank main.py, about to start debugging

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,62 +3,87 @@
 -total num months: total count rows
 -net total profits/losses: sum column B
 -changes in profits/losses with avg: diff between column B of current row and that of row before with avg diff
--greatest increase in profits: max of changes in profits/losses
--greatest decrease in losses: min of changes in profits/losses
+-greatest increase in profits: max of change in profits/losses
+-greatest decrease in losses: min of change in profits/losses
 -output to python-challenge/PyBank/PyBank_results.txt
 """
 import os
 import csv
 
 #create input variable and store path to source csv
-budget_data_csv = os.path.join("..", "Resources", "PyBank", "budget_data.csv")
+budget_data_csv_path = os.path.join("..", "Resources", "PyBank", "budget_data.csv")
 
 #create output variable and store path to .txt file
 writing_path = os.path.join("PyBank_results.txt")
 
 #define function to output data, takes a list of results with desired data in different indices
-def budget_analysis(budget_data):
+def budget_analysis(budget_data_list):
     print(f"Financial Analysis\n")
     print(f"-------------------------------\n")
-    print(f"Total Months: {budget_data[0]}\n")
-    print(f"Total: ${budget_data[1]}\n")
-    print(f"Average Change: ${budget_data[2]}\n")
-    print(f"Greatest Increase in profits: {budget_data[3]} (${budget_data[4]})\n")
-    print(f"Greatest Decrease in profits: {budget_data[5]} (${budget_data[6]})\n")
+    print(f"Total Months: {budget_data_list[0]}\n")
+    print(f"Total: ${budget_data_list[1]}\n")
+    print(f"Average Change: ${budget_data_list[2]}\n")
+    print(f"Greatest Increase in profits: {budget_data_list[3]} (${budget_data_list[4]})\n")
+    print(f"Greatest Decrease in profits: {budget_data_list[5]} (${budget_data_list[6]})\n")
     print(f"-----------------------------------------------------------------------------\n")
     
 #pass in path variable and open up csv file for reading
-with open(budget_data_csv, 'r') as csvFile:
+with open(budget_data_csv_path, 'r') as csvFile:
 
 #specify a reader using commas as delimiter
     csvReader = csv.reader(csvFile, delimiter=',')
 
-#declare list
+#declare lists and variables and initialize if needed
     data_results = []
-    list_of_diffs = []
+    list_of_changes = []
+    total_profits_losses = 0
 
-#declare variables to help chop off header and initialize previous row profit/loss variable
-    header_counter = 1
-    prev_ro_PorL = 0
 #for loop to iterate through rows in csv file
     for row in csvReader:
-        if  header_counter == 1:
-            header = "Header: " + row[0] + row[1]
-        else: 
-            list_of_diffs[row - 2] = int(row[1]) - prev_row_PorL 
-            data_results[0] += 1
-            data_results[1] += int(row[1])
-            prev_row_PorL = int(row[1])
-    header_counter += 1
- 
- #for testing only
-data_results[3] = 11231985
-data_results[4] = 11231985
-data_results[5] = 11231985
-data_results[6] = 11231985
 
-for diff in list_of_diffs:
-    print(f"{diff}\n")
+#chop off header
+        if  (row == 1):
+            header = row[0] + row[1]
+        else: 
+
+#fill in list of changes from row to row in the second column, starting with list_of_changes[0] receiving the 
+#difference between the second and first rows 
+#run a sum on the profits/losses column           
+            list_of_changes[row - 2] = (row[1]) - ((row-1)[1])
+            total_profits_losses += (row[1]) 
+#keep track of max and min change, and the associated month/year - if no change in max/min do nothing
+            if (row > 2 & list_of_changes[row - 2] > list_of_changes[row - 3]):
+                max_change = list_of_changes[row - 2]
+                max_month_year = row[0]
+            elif (row > 2 & list_of_changes[row - 2] < list_of_changes[row - 3]):
+                min_change = list_of_changes[row - 2]
+                min_month_year = row[0] 
+            else:
+                min_month_year = min_month_year   
+
+#store sum of profits/losses
+    data_results[0] = total_profits_losses 
+
+#find average of change in profits/losses and store
+    profit_loss_avg_change = sum(list_of_changes) / len(list_of_changes)
+    data_results[2] = profit_loss_avg_change
+
+#get row count (one more than number of differences) and store it
+    total_months = len(list_of_changes) + 1
+    data_results[1] = total_months
+
+#store max_change and associated month/year
+    data_results[4] = max_change
+    data_results[3] = max_month_year
+
+#store min change and associated month/year 
+    data_results[6] = min_change
+    data_results[5] = min_month_year
+ 
+#for testing only
+for change in list_of_changes:
+    print(f"{change}\n")
+
 budget_analysis(data_results)
     
 # #pass function output to a string
