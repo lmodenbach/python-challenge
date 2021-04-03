c5b044a469beed00859601a79e7e3d3ba9d60bde
lmodenbach <lmodenbach@gmail.com>
2021-04-03 14:22:00 -0700

    finishing touches
    removed notes to self

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,3 @@
-"""Objectives:
--read in from python-challenge/Resources/PyBank/budget_data.csv
--total num months: total count rows
--net total profits/losses: sum column B
--changes in profits/losses with avg: diff between column B of current row and that of row before with avg diff
--greatest increase in profits: max of change in profits/losses
--greatest decrease in losses: min of change in profits/losses
--output to python-challenge/PyBank/PyBank_results.txt
-"""
 import os
 import csv
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,3 @@
-"""Objectives:
--read in from python-challenge/Resources/PyPoll/election_data.csv
--total num votes cast: count of rows
--list of candidates: list of unique string values in column C
--total num of votes per candidate: count of instances per each unique value in C
--percentage of vote won by candidate: (each count of instances over row count)*100
--winner of election based on pop vote: max of units of a data structure holding count of instances
-    of unique values in C
--output results to python-challenge/PyPoll/PyPoll_results.txt
- """
-
 import os
 import csv
 
